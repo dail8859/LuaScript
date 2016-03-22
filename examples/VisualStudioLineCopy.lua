711e856077f798b96105de0653fcc84e0d23a529
dail8859 <dail8859@users.noreply.github.com>
2016-03-22 12:26:25 -0400

    Create VisualStudioLineCopy.lua

---
+++ examples/VisualStudioLineCopy.lua
@@ -0,0 +1,14 @@
+-- Mimic Visual Studio's "Ctrl+C" that copies the entire line if nothing is selected
+npp.AddShortcut("Copy Allow Line", "Ctrl+C", function()
+	editor:CopyAllowLine()
+end)
+
+-- Mimic Visual Studio's "Ctrl+X" that cuts the line if nothing is selected
+npp.AddShortcut("Cut Allow Line", "Ctrl+X", function()
+	if editor.SelectionEmpty then
+		editor:CopyAllowLine()
+		editor:LineDelete()
+	else
+		editor:Cut()
+	end
+end)
