dail8859/LuaScript

commit 9ee00a265b32ad2380ace69e753ac38cf0494619

Move more logic into IFaceTable

IFaceTableMixer.cpp
@@ -1,0 +1,74 @@
+// This file is part of LuaScript.
+// 
+// Copyright (C)2016 Justin Dailey <dail8859@yahoo.com>
+// 
+// LuaScript is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#include "IFaceTableMixer.h"
+
+const IFaceConstant *IFaceTableMixer::FindConstant(const char *name) {
+	for (auto const &iface : ifaces) {
+		const IFaceConstant *ret = iface->FindConstant(name);
+		if (ret != nullptr) return ret;
+	}
+	return nullptr;
+}
+
+const IFaceFunction *IFaceTableMixer::FindFunction(const char *name) {
+	for (auto const &iface : ifaces) {
+		const IFaceFunction *ret = iface->FindFunction(name);
+		if (ret != nullptr) return ret;
+	}
+	return nullptr;
+}
+
+const IFaceFunction *IFaceTableMixer::FindFunctionByConstantName(const char *name) {
+	for (auto const &iface : ifaces) {
+		const IFaceFunction *ret = iface->FindFunctionByConstantName(name);
+		if (ret != nullptr) return ret;
+	}
+	return nullptr;
+}
+
+const IFaceProperty *IFaceTableMixer::FindProperty(const char *name) {
+	for (auto const &iface : ifaces) {
+		const IFaceProperty *ret = iface->FindProperty(name);
+		if (ret != nullptr) return ret;
+	}
+	return nullptr;
+}
+
+int IFaceTableMixer::GetConstantName(int value, char *nameOut, unsigned nameBufferLen, const char *hint) {
+	for (auto const &iface : ifaces) {
+		int ret = iface->GetConstantName(value, nameOut, nameBufferLen, hint);
+		if (ret != 0) return ret;
+	}
+	return 0;
+}
+
+const IFaceFunction *IFaceTableMixer::GetFunctionByMessage(int message) {
+	for (auto const &iface : ifaces) {
+		const IFaceFunction *ret = iface->GetFunctionByMessage(message);
+		if (ret != nullptr) return ret;
+	}
+	return nullptr;
+}
+const IFaceFunction *IFaceTableMixer::GetPropertyFuncByMessage(int message) {
+	for (auto const &iface : ifaces) {
+		const IFaceFunction *ret = iface->GetPropertyFuncByMessage(message);
+		if (ret != nullptr) return ret;
+	}
+	return nullptr;
+}

IFaceTableMixer.h
@@ -1,0 +1,42 @@
+// This file is part of LuaScript.
+// 
+// Copyright (C)2016 Justin Dailey <dail8859@yahoo.com>
+// 
+// LuaScript is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#ifndef IFACETABLEMIXER_H
+#define IFACETABLEMIXER_H
+
+#include "IFaceTable.h"
+#include <vector>
+
+class IFaceTableMixer : public IFaceTableInterface {
+private:
+	std::vector<IFaceTableInterface *> ifaces;
+
+public:
+	void AddIFaceTable(IFaceTableInterface *iface) { ifaces.push_back(iface); }
+
+	// IFaceTableInterface
+	const IFaceConstant *FindConstant(const char *name);
+	const IFaceFunction *FindFunction(const char *name);
+	const IFaceFunction *FindFunctionByConstantName(const char *name);
+	const IFaceProperty *FindProperty(const char *name);
+	int GetConstantName(int value, char *nameOut, unsigned nameBufferLen, const char *hint);
+	const IFaceFunction *GetFunctionByMessage(int message);
+	const IFaceFunction *GetPropertyFuncByMessage(int message);
+};
+
+#endif

SciTE/IFaceTable.cpp
@@ -9,7 +9,7 @@
 #include <string.h>
 #include <ctype.h>
 
-int IFaceTable::FindConstant(const char *name) {
+const IFaceConstant *IFaceTable::FindConstant(const char *name) {
 	int lo = 0;
 	int hi = constantCount - 1;
 	do {
@@ -21,14 +21,14 @@ int IFaceTable::FindConstant(const char *name) {
 		} else if (cmp < 0) {
 			hi = idx - 1;
 		} else {
-			return idx;
+			return &constants[idx];
 		}
 	} while (lo <= hi);
 
-	return -1;
+	return nullptr;
 }
 
-int IFaceTable::FindFunction(const char *name) {
+const IFaceFunction *IFaceTable::FindFunction(const char *name) {
 	int lo = 0;
 	int hi = functionCount - 1;
 	do {
@@ -39,14 +39,14 @@ int IFaceTable::FindFunction(const char *name) {
 		} else if (cmp < 0) {
 			hi = idx - 1;
 		} else {
-			return idx;
+			return &functions[idx];
 		}
 	} while (lo <= hi);
 
-	return -1;
+	return nullptr;
 }
 
-int IFaceTable::FindFunctionByConstantName(const char *name) {
+const IFaceFunction *IFaceTable::FindFunctionByConstantName(const char *name) {
 	if (strncmp(name, prefix, strlen(prefix))==0) {
 		// This looks like a constant for an iface function.  This requires
 		// a sequential search.  Take special care since the function names
@@ -60,14 +60,14 @@ int IFaceTable::FindFunctionByConstantName(const char *name) {
 				++fn;
 			}
 			if (!*nm && !*fn) {
-				return idx;
+				return &functions[idx];
 			}
 		}
 	}
-	return -1;
+	return nullptr;
 }
 
-int IFaceTable::FindProperty(const char *name) {
+const IFaceProperty *IFaceTable::FindProperty(const char *name) {
 	int lo = 0;
 	int hi = propertyCount - 1;
 	do {
@@ -79,11 +79,11 @@ int IFaceTable::FindProperty(const char *name) {
 		} else if (cmp < 0) {
 			hi = idx - 1;
 		} else {
-			return idx;
+			return &properties[idx];
 		}
 	} while (lo <= hi);
 
-	return -1;
+	return nullptr;
 }
 
 int IFaceTable::GetConstantName(int value, char *nameOut, unsigned nameBufferLen, const char *hint) {
@@ -124,4 +124,25 @@ int IFaceTable::GetConstantName(int value, char *nameOut, unsigned nameBufferLen
 	}
 
 	return 0;
+}
+
+const IFaceFunction *IFaceTable::GetFunctionByMessage(int message) {
+	for (int funcIdx = 0; funcIdx < functionCount; ++funcIdx) {
+		if (functions[funcIdx].value == message) {
+			return &functions[funcIdx];
+		}
+	}
+	return nullptr;
+}
+
+const IFaceFunction *IFaceTable::GetPropertyFuncByMessage(int message) {
+	for (int propIdx = 0; propIdx < propertyCount; ++propIdx) {
+		if (properties[propIdx].getter == message) {
+			return &properties[propIdx].GetterFunction();
+		}
+		else if (properties[propIdx].setter == message) {
+			return &properties[propIdx].SetterFunction();
+		}
+	}
+	return nullptr;
 }

SciTE/IFaceTable.h
@@ -18,7 +18,7 @@ enum IFaceType {
 	iface_keymod,
 	iface_string,
 	iface_stringresult,
-	iface_cells,
+	iface_cells, // This and everything else below is not "scriptable"
 	iface_textrange,
 	iface_findtext,
 	iface_formatrange
@@ -70,11 +70,13 @@ struct IFaceProperty {
 
 class IFaceTableInterface {
 public:
-	virtual int FindConstant(const char *name) = 0;
-	virtual int FindFunction(const char *name) = 0;
-	virtual int FindFunctionByConstantName(const char *name) = 0;
-	virtual int FindProperty(const char *name) = 0;
+	virtual const IFaceConstant *FindConstant(const char *name) = 0;
+	virtual const IFaceFunction *FindFunction(const char *name) = 0;
+	virtual const IFaceFunction *FindFunctionByConstantName(const char *name) = 0;
+	virtual const IFaceProperty *FindProperty(const char *name) = 0;
 	virtual int GetConstantName(int value, char *nameOut, unsigned nameBufferLen, const char *hint) = 0;
+	virtual const IFaceFunction *GetFunctionByMessage(int message) = 0;
+	virtual const IFaceFunction *GetPropertyFuncByMessage(int message) = 0;
 };
 
 class IFaceTable : public IFaceTableInterface {
@@ -101,12 +103,14 @@ public:
 	const int constantCount;
 	const int propertyCount;
 
-	int FindConstant(const char *name);
-	int FindFunction(const char *name);
-	int FindFunctionByConstantName(const char *name);
-	int FindProperty(const char *name);
-
+	// IFaceTableInterface
+	const IFaceConstant *FindConstant(const char *name);
+	const IFaceFunction *FindFunction(const char *name);
+	const IFaceFunction *FindFunctionByConstantName(const char *name);
+	const IFaceProperty *FindProperty(const char *name);
 	int GetConstantName(int value, char *nameOut, unsigned nameBufferLen, const char *hint);
+	const IFaceFunction *GetFunctionByMessage(int message);
+	const IFaceFunction *GetPropertyFuncByMessage(int message);
 };
 
 #endif

SciTE/LuaExtension.cpp
@@ -18,10 +18,15 @@
 #include "StyleWriter.h"
 #include "NppExtensionAPI.h"
 #include "LuaExtension.h"
-#include "SciteIFaceTable.h"
 #include "WcharMbcsConverter.h"
 
+#include "IFaceTableMixer.h"
+#include "SciteIFaceTable.h"
+#include "NppIFaceTable.h"
+
 #include "lua.hpp"
+
+IFaceTableMixer ifacemixer;
 
 // From lua.c
 #define EOFMARK         "<eof>"
@@ -230,35 +235,22 @@ static int cf_npp_send(lua_State *L) {
 	lua_pushvalue(L, paneIndex);
 	lua_replace(L, 1);
 
-	IFaceFunction func = { "", 0, iface_void, {iface_void, iface_void} };
-	for (int funcIdx = 0; funcIdx < SciteIFaceTable.functionCount; ++funcIdx) {
-		if (SciteIFaceTable.functions[funcIdx].value == message) {
-			func = SciteIFaceTable.functions[funcIdx];
-			break;
-		}
+	const IFaceFunction *func;
+	func = ifacemixer.GetFunctionByMessage(message);
+
+	if (func == nullptr) {
+		func = ifacemixer.GetPropertyFuncByMessage(message);
 	}
 
-	if (func.value == 0) {
-		for (int propIdx = 0; propIdx < SciteIFaceTable.propertyCount; ++propIdx) {
-			if (SciteIFaceTable.properties[propIdx].getter == message) {
-				func = SciteIFaceTable.properties[propIdx].GetterFunction();
-				break;
-			} else if (SciteIFaceTable.properties[propIdx].setter == message) {
-				func = SciteIFaceTable.properties[propIdx].SetterFunction();
-				break;
-			}
-		}
-	}
-
-	if (func.value != 0) {
-		if (IFaceFunctionIsScriptable(func)) {
-			return iface_function_helper(L, func);
+	if (func != nullptr) {
+		if (IFaceFunctionIsScriptable(*func)) {
+			return iface_function_helper(L, *func);
 		} else {
 			raise_error(L, "Cannot call send for this function: not scriptable.");
 			return 0;
 		}
 	} else {
-		raise_error(L, "Message number does not match any published Scintilla function or property");
+		raise_error(L, "Message number does not match any published Scintilla / Notepad++ function or property");
 		return 0;
 	}
 }
@@ -270,7 +262,7 @@ static int cf_npp_constname(lua_State *L) {
 
 	hint = luaL_optstring(L, 2, nullptr);
 
-	if (SciteIFaceTable.GetConstantName(message, constName, 100, hint) > 0) {
+	if (ifacemixer.GetConstantName(message, constName, 100, hint) > 0) {
 		lua_pushstring(L, constName);
 		return 1;
 	} else {
@@ -1148,10 +1140,10 @@ static int cf_pane_iface_function(lua_State *L) {
 }
 
 static int push_iface_function(lua_State *L, const char *name) {
-	int i = SciteIFaceTable.FindFunction(name);
-	if (i >= 0) {
-		if (IFaceFunctionIsScriptable(SciteIFaceTable.functions[i])) {
-			lua_pushlightuserdata(L, const_cast<IFaceFunction *>(SciteIFaceTable.functions + i));
+	auto func= ifacemixer.FindFunction(name);
+	if (func != nullptr) {
+		if (IFaceFunctionIsScriptable(*func)) {
+			lua_pushlightuserdata(L, (void*)func);
 			lua_pushcclosure(L, cf_pane_iface_function, 1);
 
 			// Since Lua experts say it is inefficient to create closures / cfunctions
@@ -1168,32 +1160,31 @@ static int push_iface_function(lua_State *L, const char *name) {
 static int push_iface_propval(lua_State *L, const char *name) {
 	// this function doesn't raise errors, but returns 0 if the function is not handled.
 
-	int propidx = SciteIFaceTable.FindProperty(name);
-	if (propidx >= 0) {
-		const IFaceProperty &prop = SciteIFaceTable.properties[propidx];
-		if (!IFacePropertyIsScriptable(prop)) {
+	auto prop = ifacemixer.FindProperty(name);
+	if (prop != nullptr) {
+		if (!IFacePropertyIsScriptable(*prop)) {
 			raise_error(L, "Error: iface property is not scriptable.");
 			return -1;
 		}
 
-		if (prop.paramType == iface_void) {
-			if (prop.getter) {
+		if (prop->paramType == iface_void) {
+			if (prop->getter) {
 				lua_settop(L, 1);
-				return iface_function_helper(L, prop.GetterFunction());
+				return iface_function_helper(L, prop->GetterFunction());
 			}
-		} else if (prop.paramType == iface_bool) {
+		} else if (prop->paramType == iface_bool) {
 			// The bool getter is untested since there are none in the iface.
 			// However, the following is suggested as a reference protocol.
 			NppExtensionAPI::Pane p = check_pane_object(L, 1);
 
-			if (prop.getter) {
-				if (host->Send(p, prop.getter, 1, 0)) {
+			if (prop->getter) {
+				if (host->Send(p, prop->getter, 1, 0)) {
 					lua_pushnil(L);
 					return 1;
 				} else {
 					lua_settop(L, 1);
 					lua_pushboolean(L, 0);
-					return iface_function_helper(L, prop.GetterFunction());
+					return iface_function_helper(L, prop->GetterFunction());
 				}
 			}
 		} else {
@@ -1206,7 +1197,7 @@ static int push_iface_propval(lua_State *L, const char *name) {
 			IFacePropertyBinding *ipb = static_cast<IFacePropertyBinding *>(lua_newuserdata(L, sizeof(IFacePropertyBinding)));
 			if (ipb) {
 				ipb->pane = check_pane_object(L, 1);
-				ipb->prop = &prop;
+				ipb->prop = prop;
 				if (luaL_newmetatable(L, "Npp_MT_IFacePropertyBinding")) {
 					lua_pushliteral(L, "__index");
 					lua_pushcfunction(L, cf_ifaceprop_metatable_index);
@@ -1255,18 +1246,15 @@ static int cf_pane_metatable_index(lua_State *L) {
 
 static int cf_pane_metatable_newindex(lua_State *L) {
 	if (lua_isstring(L, 2)) {
-		int propidx = SciteIFaceTable.FindProperty(lua_tostring(L, 2));
-		if (propidx >= 0) {
-			const IFaceProperty &prop = SciteIFaceTable.properties[propidx];
-			if (IFacePropertyIsScriptable(prop)) {
-				if (prop.setter) {
+		auto prop = ifacemixer.FindProperty(lua_tostring(L, 2));
+		if (prop != nullptr) {
+			if (IFacePropertyIsScriptable(*prop)) {
+				if (prop->setter) {
 					// stack needs to be rearranged to look like an iface function call
 					lua_remove(L, 2);
-
-					if (prop.paramType == iface_void) {
-						return iface_function_helper(L, prop.SetterFunction());
-
-					} else if ((prop.paramType == iface_bool)) {
+					if (prop->paramType == iface_void) {
+						return iface_function_helper(L, prop->SetterFunction());
+					} else if ((prop->paramType == iface_bool)) {
 						if (!lua_isnil(L, 3)) {
 							lua_pushboolean(L, 1);
 							lua_insert(L, 2);
@@ -1275,7 +1263,7 @@ static int cf_pane_metatable_newindex(lua_State *L) {
 							// just push an arbitrary numeric value that Scintilla will ignore
 							lua_pushinteger(L, 0);
 						}
-						return iface_function_helper(L, prop.SetterFunction());
+						return iface_function_helper(L, prop->SetterFunction());
 
 					} else {
 						raise_error(L, "Error - (pane object) cannot assign directly to indexed property");
@@ -1328,14 +1316,14 @@ static int cf_global_metatable_index(lua_State *L) {
 			return 0;
 		}
 
-		int i = SciteIFaceTable.FindConstant(name);
-		if (i >= 0) {
-			lua_pushinteger(L, SciteIFaceTable.constants[i].value);
+		auto con = ifacemixer.FindConstant(name);
+		if (con != nullptr) {
+			lua_pushinteger(L, con->value);
 			return 1;
 		} else {
-			i = SciteIFaceTable.FindFunctionByConstantName(name);
-			if (i >= 0) {
-				lua_pushinteger(L, SciteIFaceTable.functions[i].value);
+			auto func = ifacemixer.FindFunctionByConstantName(name);
+			if (func != nullptr) {
+				lua_pushinteger(L, func->value);
 
 				// FindFunctionByConstantName is slow, so cache the result into the
 				// global table.  My tests show this gives an order of magnitude
@@ -1435,6 +1423,9 @@ static bool InitGlobalScope(bool checkProperties, bool forceReload = false) {
 	} else {
 		return false;
 	}
+
+	ifacemixer.AddIFaceTable(&SciteIFaceTable);
+	//ifacemixer.AddIFaceTable(&NppIFaceTable);
 
 	// ...register standard libraries
 	luaL_openlibs(luaState);

LuaScript.vcxproj
@@ -119,8 +119,10 @@
     <ClCompile Include="Dialogs\ConsoleDialog.cpp" />
     <ClCompile Include="Dialogs\Hyperlinks.cpp" />
     <ClCompile Include="Dialogs\StaticDialog.cpp" />
+    <ClCompile Include="IFaceTableMixer.cpp" />
     <ClCompile Include="NppExtensionAPI.cpp" />
     <ClCompile Include="LuaScript.cpp" />
+    <ClCompile Include="NppIFaceTable.cpp" />
     <ClCompile Include="SciteIFaceTable.cpp" />
     <ClCompile Include="SciTE\GUIWin.cpp" />
     <ClCompile Include="SciTE\IFaceTable.cpp" />
@@ -137,7 +139,9 @@
     <ClInclude Include="Dialogs\resource.h" />
     <ClInclude Include="Dialogs\StaticDialog.h" />
     <ClInclude Include="Dialogs\Window.h" />
+    <ClInclude Include="IFaceTableMixer.h" />
     <ClInclude Include="NppExtensionAPI.h" />
+    <ClInclude Include="NppIFaceTable.h" />
     <ClInclude Include="Npp\Docking.h" />
     <ClInclude Include="Npp\DockingDlgInterface.h" />
     <ClInclude Include="Npp\dockingResource.h" />

LuaScript.vcxproj.filters
@@ -51,6 +51,12 @@
     <ClCompile Include="SciteIFaceTable.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
+    <ClCompile Include="NppIFaceTable.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="IFaceTableMixer.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="PluginDefinition.h">
@@ -126,6 +132,12 @@
       <Filter>Header Files</Filter>
     </ClInclude>
     <ClInclude Include="SciteIFaceTable.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="NppIFaceTable.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="IFaceTableMixer.h">
       <Filter>Header Files</Filter>
     </ClInclude>
   </ItemGroup>

NppIFaceTable.cpp
@@ -1,0 +1,54 @@
+// This file is part of LuaScript.
+// 
+// Copyright (C)2016 Justin Dailey <dail8859@yahoo.com>
+// 
+// LuaScript is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#include "IFaceTable.h"
+#include "Notepad_plus_msgs.h"
+
+static IFaceConstant ifaceConstants[] = {
+	{ "WV_95", WV_95 },
+	{ "WV_98", WV_98 },
+	{ "WV_ME", WV_ME },
+	{ "WV_NT", WV_NT },
+	{ "WV_S2003", WV_S2003 },
+	{ "WV_UNKNOWN", WV_UNKNOWN },
+	{ "WV_VISTA", WV_VISTA },
+	{ "WV_W2K", WV_W2K },
+	{ "WV_WIN32S", WV_WIN32S },
+	{ "WV_WIN7", WV_WIN7 },
+	{ "WV_WIN8", WV_WIN8 },
+	{ "WV_WIN81", WV_WIN81 },
+	{ "WV_XP", WV_XP },
+	{ "WV_XPX64", WV_XPX64 },
+};
+
+static IFaceFunction ifaceFunctions[] = {
+	{ "SaveCurrentFile", NPPM_SAVECURRENTFILE, iface_void, { iface_void, iface_void } }
+};
+
+static IFaceProperty ifaceProperties[] = {
+	{ "CurrentLangType ", NPPM_GETCURRENTLANGTYPE, NPPM_SETCURRENTLANGTYPE, iface_int, iface_void },
+	{ "WindowsVersion ", NPPM_GETWINDOWSVERSION, 0, iface_int, iface_void }
+};
+
+enum {
+	ifaceFunctionCount = 1,
+	ifaceConstantCount = 14,
+	ifacePropertyCount = 2
+};
+
+IFaceTable NppIFaceTable("NPPM_", ifaceFunctions, ifaceFunctionCount, ifaceConstants, ifaceConstantCount, ifaceProperties, ifacePropertyCount);

commit c5097017d7ca06623b7937819f8c4b418426e3da

Fix issue introduced in 9ee00a2
The IFaceProperty was returning a local struct but the caller was using the address. Not the best solution currently but good enough for now. Closes #27

IFaceTableMixer.cpp
@@ -65,10 +65,11 @@ const IFaceFunction *IFaceTableMixer::GetFunctionByMessage(int message) {
 	}
 	return nullptr;
 }
-const IFaceFunction *IFaceTableMixer::GetPropertyFuncByMessage(int message) {
+
+IFaceFunction IFaceTableMixer::GetPropertyFuncByMessage(int message) {
 	for (auto const &iface : ifaces) {
-		const IFaceFunction *ret = iface->GetPropertyFuncByMessage(message);
-		if (ret != nullptr) return ret;
+		IFaceFunction ret = iface->GetPropertyFuncByMessage(message);
+		if (ret.value != -1) return ret;
 	}
-	return nullptr;
+	return{ "invalid", -1, iface_void, { iface_void, iface_void } };
 }

IFaceTableMixer.h
@@ -36,7 +36,7 @@ public:
 	const IFaceProperty *FindProperty(const char *name);
 	int GetConstantName(int value, char *nameOut, unsigned nameBufferLen, const char *hint);
 	const IFaceFunction *GetFunctionByMessage(int message);
-	const IFaceFunction *GetPropertyFuncByMessage(int message);
+	IFaceFunction GetPropertyFuncByMessage(int message);
 };
 
 #endif

SciTE/IFaceTable.cpp
@@ -135,14 +135,14 @@ const IFaceFunction *IFaceTable::GetFunctionByMessage(int message) {
 	return nullptr;
 }
 
-const IFaceFunction *IFaceTable::GetPropertyFuncByMessage(int message) {
+IFaceFunction IFaceTable::GetPropertyFuncByMessage(int message) {
 	for (int propIdx = 0; propIdx < propertyCount; ++propIdx) {
 		if (properties[propIdx].getter == message) {
-			return &properties[propIdx].GetterFunction();
+			return properties[propIdx].GetterFunction();
 		}
 		else if (properties[propIdx].setter == message) {
-			return &properties[propIdx].SetterFunction();
+			return properties[propIdx].SetterFunction();
 		}
 	}
-	return nullptr;
+	return { "invalid", -1, iface_void, {iface_void, iface_void} };
 }

SciTE/IFaceTable.h
@@ -106,7 +106,7 @@ public:
 	virtual const IFaceProperty *FindProperty(const char *name) = 0;
 	virtual int GetConstantName(int value, char *nameOut, unsigned nameBufferLen, const char *hint) = 0;
 	virtual const IFaceFunction *GetFunctionByMessage(int message) = 0;
-	virtual const IFaceFunction *GetPropertyFuncByMessage(int message) = 0;
+	virtual IFaceFunction GetPropertyFuncByMessage(int message) = 0;
 };
 
 class IFaceTable : public IFaceTableInterface {
@@ -140,7 +140,7 @@ public:
 	const IFaceProperty *FindProperty(const char *name);
 	int GetConstantName(int value, char *nameOut, unsigned nameBufferLen, const char *hint);
 	const IFaceFunction *GetFunctionByMessage(int message);
-	const IFaceFunction *GetPropertyFuncByMessage(int message);
+	IFaceFunction GetPropertyFuncByMessage(int message);
 };
 
 #endif

SciTE/LuaExtension.cpp
@@ -213,6 +213,7 @@ static int cf_npp_send(lua_State *L) {
 	// It is rewritten to be typesafe, checking the arguments against the metadata in
 	// IFaceTable in the same way that the object interface does.
 
+	IFaceFunction propfunc;
 	int paneIndex = lua_upvalueindex(1);
 	check_pane_object(L, paneIndex);
 	int message = (int)luaL_checkinteger(L, 1);
@@ -224,7 +225,8 @@ static int cf_npp_send(lua_State *L) {
 	func = SciIFaceTable.GetFunctionByMessage(message);
 
 	if (func == nullptr) {
-		func = SciIFaceTable.GetPropertyFuncByMessage(message);
+		propfunc = SciIFaceTable.GetPropertyFuncByMessage(message);
+		if (propfunc.value != -1) func = &propfunc;
 	}
 
 	if (func != nullptr) {