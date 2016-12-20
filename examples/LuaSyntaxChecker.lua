d0c3128549a4c5fb0b624e3f66bfc2a6db6e0774
OwnageIsMagic <OwnageIsMagic@users.noreply.github.com>
2016-12-20 20:15:07 +0300

    Update LuaSyntaxChecker.lua
    add colors to error msg

--- examples/LuaSyntaxChecker.lua
+++ examples/LuaSyntaxChecker.lua
@@ -1,8 +1,8 @@
 -- Any time a lua file is saved, this compiles it and checks for
 -- syntax errors. Errors are added as inline annotations
 
-editor1.AnnotationVisible = ANNOTATION_INDENTED
-editor2.AnnotationVisible = ANNOTATION_INDENTED
+editor1.AnnotationVisible = ANNOTATION_BOXED
+editor2.AnnotationVisible = ANNOTATION_BOXED
 
 npp.AddEventHandler("OnSave", function(filename, bufferid)
     if npp:GetExtPart() ~= ".lua" then return false end
@@ -10,13 +10,14 @@ npp.AddEventHandler("OnSave", function(filename, bufferid)
     editor:AnnotationClearAll()
 
     -- Try to compile the lua code
-    local f, err = load(editor:GetText(0, editor.Length), npp:GetFileName(), "t")
+    local _, err = load(editor:GetText(0, editor.Length), npp:GetFileName(), "t")
 
     -- See if it worked
-    if f ~= nil then return false end
+    if err == nil then return false end
 
     print(err)
-    editor.AnnotationText[tonumber(err:match(":(%d+):")) - 1] = err
-
+    local line = tonumber(err:match(":(%d+):")) - 1
+    editor.AnnotationText[line] = err
+    editor.AnnotationStyle[line] = STYLE_BRACELIGHT
     return false
 end)
