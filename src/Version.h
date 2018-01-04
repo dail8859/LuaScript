a9f1a9cad932bdaeb5cf275387862f235fb07398
dail8859 <dail8859@yahoo.com>
2018-01-04 09:02:32 -0500

    Bump version to v0.7.1

--- src/Version.h
+++ src/Version.h
@@ -16,8 +16,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-#define VERSION_NUM				0,7,0,0
-#define VERSION_LINEAR			700
-#define VERSION_LINEAR_TEXT		TEXT("700")
-#define VERSION_TEXT			TEXT("0.7.0") // This must match the tag pushed on the server minus the "v"
+#define VERSION_NUM				0,7,1,0
+#define VERSION_LINEAR			710
+#define VERSION_LINEAR_TEXT		TEXT("710")
+#define VERSION_TEXT			TEXT("0.7.1") // This must match the tag pushed on the server minus the "v"
 #define VERSION_STAGE			TEXT("") // "alpha", "beta", ""
